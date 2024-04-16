f5c8444263e5aee7982217edc5659e50efc03ac8
AnthonyChuang <anthonymchuang@gmail.com>
2024-04-15 23:26:59 -0500

    added basic login UI

---
+++ VirtualStudyRoom/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VirtualStudyRoom
+//
+//  Created by Anthony Chuang on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        // You would add your login logic here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
