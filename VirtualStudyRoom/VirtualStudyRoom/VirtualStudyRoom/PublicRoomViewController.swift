ac850efcd3772a1883971fb899eee4333b0cba80
AnthonyChuang <85719130+AnthonyChuang@users.noreply.github.com>
2024-05-03 00:54:01 -0500

    Add files via upload

--- VirtualStudyRoom/VirtualStudyRoom/VirtualStudyRoom/PublicRoomViewController.swift
+++ VirtualStudyRoom/VirtualStudyRoom/VirtualStudyRoom/PublicRoomViewController.swift
@@ -6,20 +6,181 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PublicRoomViewController: UIViewController, RoomSettingsPopupDelegate, UITextFieldDelegate {
-    
-    @IBOutlet weak var timerTextField: UITextField!
-    @IBOutlet weak var startTimerButton: UIButton!
-    
-    var pomodoroTimer: Timer?
-    var timeLeft: TimeInterval = 25 * 60 // Default 25 minutes
+
     
     @IBOutlet weak var roomNameLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var timer: Timer?
+    var workInterval: TimeInterval = 0.2 * 60
+    var restInterval: TimeInterval = 0.1 * 60
+    var isWorkTime = true
+    var remainingTime: TimeInterval = 0
+    
+    var audioPlayer: AVAudioPlayer?
+    var songs = [String]() // This will hold the names of your mp3 files
+    var currentSongIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timerTextField.delegate = self
+        
+        songs = [
+            "lofisong.mp3"
+        ]
+
+        
+        remainingTime = workInterval // Start with the work interval
+        setupBackground()
+        setupStartStopButton()
+        setupResetButton()
+    }
+    
+    func setupAudioPlayer() {
+        let songName = songs[currentSongIndex]
+        guard let url = Bundle.main.url(forResource: "Songs/\(songName)", withExtension: nil) else {
+            print("Song file \(songName) not found.")
+            proceedToNextSong()
+            return
+        }
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.delegate = self
+            audioPlayer?.prepareToPlay()
+            playMusic()
+        } catch let error {
+            print("Could not load file: \(error.localizedDescription)")
+            proceedToNextSong()
+        }
+    }
+
+
+
+    
+    func setupResetButton() {
+        // Set the button's appearance to be circular and filled
+        resetButton.layer.cornerRadius = resetButton.bounds.size.height / 2
+        resetButton.backgroundColor = UIColor.black
+        resetButton.setTitleColor(UIColor.white, for: .normal)
+        resetButton.setTitle("Reset", for: .normal) // Set the default state title
+        resetButton.clipsToBounds = true
+    }
+
+    
+    func setupStartStopButton() {
+        // Set the button's appearance to be circular and filled
+        startStopButton.layer.cornerRadius = startStopButton.bounds.size.height / 2
+        startStopButton.backgroundColor = UIColor.black
+        startStopButton.setTitleColor(UIColor.white, for: .normal)
+        startStopButton.setTitle("Start", for: .normal) // Set the default state title
+        startStopButton.clipsToBounds = true
+    }
+    
+    
+    func setupBackground(){
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+           backgroundImage.image = UIImage(named: "Background")
+           backgroundImage.contentMode = .scaleAspectFill // or .scaleAspectFit
+           view.insertSubview(backgroundImage, at: 0) // Ensure the image is behind other subviews
+    }
+    @IBAction func resetTimerTapped(_ sender: UIButton) {
+        if isWorkTime {
+            remainingTime = workInterval
+        } else {
+            remainingTime = restInterval
+        }
+        updateTimerDisplay()
+    }
+    
+    func updateTimerDisplay() {
+        timerLabel.text = formatTime(seconds: remainingTime)
+    }
+    
+    func restartTimer() {
+        stopTimer() // Ensure the current timer is stopped before restarting
+        startTimer()
+    }
+    
+    @IBAction func toggleTimer(_ sender: UIButton) {
+        if timer == nil {
+            startTimer()
+            sender.setTitle("Stop", for: .normal)
+            sender.backgroundColor = UIColor.systemRed // Stop color
+        } else {
+            stopTimer()
+            sender.setTitle("Start", for: .normal)
+            sender.backgroundColor = UIColor.systemGreen // Start color
+        }
+    }
+
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        if isWorkTime {
+            setupAudioPlayer()
+            playMusic()
+        }
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        timerLabel.text = formatTime(seconds: remainingTime)
+        audioPlayer?.stop() // Stop the music when timer stops
+    }
+
+    @objc func updateTimer() {
+        if remainingTime > 0 {
+            remainingTime -= 1
+            timerLabel.text = formatTime(seconds: remainingTime)
+        } else {
+            completeTimerCycle()
+        }
+    }
+
+    func completeTimerCycle() {
+        if isWorkTime {
+            remainingTime = restInterval
+            audioPlayer?.stop()  // Stop the music during rest interval
+            showNotification(title: "Work time ended, start rest!")
+        } else {
+            remainingTime = workInterval
+            setupAudioPlayer()  // Prepare to play music for the next work cycle
+            playMusic()  // Start playing music
+            showNotification(title: "Rest time ended, back to work!")
+        }
+        isWorkTime.toggle()
+        timerLabel.text = formatTime(seconds: remainingTime)
+    }
+
+    func showNotification(title: String) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    func formatTime(seconds: TimeInterval) -> String {
+        let minutes = Int(seconds) / 60
+        let seconds = Int(seconds) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func playMusic() {
+        if !audioPlayer!.isPlaying {
+            audioPlayer?.play()
+        }
+    }
+    
+    func proceedToNextSong() {
+        currentSongIndex += 1
+        if currentSongIndex >= songs.count {
+            currentSongIndex = 0  // Loop back to the first song if at the end
+        }
+        setupAudioPlayer() // Attempt to setup the next song
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
@@ -33,45 +194,6 @@ class PublicRoomViewController: UIViewController, RoomSettingsPopupDelegate, UIT
             settingsVC.delegate = self  // Set the delegate to communicate changes back
             self.present(settingsVC, animated: true, completion: nil)
         }
-    }
-    
-    
-    @IBAction func startTimerButtonTapped(_ sender: UIButton) {
-        if let timeText = timerTextField.text, let time = TimeInterval(timeText) {
-            timeLeft = time * 60 // Convert minutes to seconds
-            timerTextField.isEnabled = false // Disable the text field while timer is running
-            startPomodoroTimer()
-        }
-    }
-    
-    func startPomodoroTimer() {
-        pomodoroTimer?.invalidate() // Invalidate any existing timer
-        pomodoroTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-    }
-    
-    @objc func updateTimer() {
-        if timeLeft > 0 {
-            timeLeft -= 1
-            updateTimerTextField()
-        } else {
-            pomodoroTimer?.invalidate()
-            timerTextField.isEnabled = true
-            alertTimeUp()
-        }
-    }
-    
-    func updateTimerTextField() {
-        let minutes = Int(timeLeft) / 60
-        let seconds = Int(timeLeft) % 60
-        timerTextField.text = String(format: "%02d:%02d", minutes, seconds)
-    }
-    
-    func alertTimeUp() {
-        let alert = UIAlertController(title: "Time's up!", message: "Your Pomodoro session has ended.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
-            self.timerTextField.text = "25" // Reset timer text field
-        })
-        present(alert, animated: true, completion: nil)
     }
     
     func didChangeRoomName(_ name: String) {
@@ -103,3 +225,15 @@ class PublicRoomViewController: UIViewController, RoomSettingsPopupDelegate, UIT
     */
 
 }
+
+extension PublicRoomViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        currentSongIndex += 1
+        if currentSongIndex >= songs.count {
+            currentSongIndex = 0 // Loop back to the first song
+        }
+        setupAudioPlayer() // Setup the next song
+        playMusic() // Play the next song
+    }
+}
+
